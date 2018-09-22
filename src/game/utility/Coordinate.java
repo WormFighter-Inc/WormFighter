4bdbaef1b8b4af1337ae63227dc54b787c96cec8
wintleh <hunter.wintle@gmail.com>
2018-09-22 13:42:17 -0400

    New utility class Coordinate
    Created new package game.utility and added Coordinate to it.

---
+++ src/game/utility/Coordinate.java
@@ -0,0 +1,26 @@
+package game.utility;
+
+import javafx.geometry.Point2D;
+
+public class Coordinate extends Point2D{
+
+	/**
+	 * Creates a new coordinate at (x, y). 
+	 * 
+	 * @param x X value of the coordinate
+	 * @param y Y value of the coordinate
+	 */
+	public Coordinate(double x, double y) {
+		super(x, y);
+	}
+	
+	/**
+	 * Creates a nicely formatted string with the coordinates. 
+	 * 
+	 * @return A formatted string containing the x and y values of the coordinate
+	 */
+	@Override
+	public String toString() {
+		return String.format("(%5.0f, ", this.getX()) + String.format("%5.0f)", this.getY());
+	}
+}
