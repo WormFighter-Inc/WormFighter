484322ae035c7e37a8f393297b9fdcf68976767a
wintleh <hunter.wintle@gmail.com>
2018-09-24 17:37:17 -0400

    Fix player

--- src/game/player/Player.java
+++ src/game/player/Player.java
@@ -1,15 +1,16 @@
-package game.ui;
+package game.player;
 
+import game.ai.Sprite;
 import javafx.scene.image.Image;
 
 public class Player extends Sprite{
 	static Image sprite = new Image("/game/sprites/player/Player.png");
 
-	Player() {
+	public Player() {
 		super(sprite);
 	}
 	
-	Player(double x, double y, int spriteWidth, int spriteHeight) {
+	public Player(double x, double y, int spriteWidth, int spriteHeight) {
 		super(x, y, spriteWidth, spriteHeight, sprite);
 	}
 
